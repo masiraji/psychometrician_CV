46b86c8dfa6b896181048f814e855cbc0a4a8cb1
masiraji <siraji1993@gmail.com>
2022-01-15 22:44:39 +0800

    Psychometrics

---
+++ script.R
@@ -0,0 +1,13 @@
+
+use_csv_data_storage()
+
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Mushfiqul Anwar Siraji",
+  data_location = "data/",
+  pdf_location = "https://github.com/masiraji/CV",
+  html_location = "https://github.com/masiraji/CV",
+  source_location = "https://github.com/masiraji/CV",
+  use_network_logo = FALSE
+)
+
