a833da478b710b557dcee29f9b3601c25acb76f4
Mushfiqul Anwar Siraji <siraji1993@gmail.com>
2022-04-11 17:38:01 +0800

    updates

--- render_cv.r
+++ render_cv.r
@@ -6,16 +6,16 @@
 # for the HTML and PDF rendering. This exercise is left to the reader.
 
 # Knit the HTML version
-rmarkdown::render("siraji_cv.rmd",
+rmarkdown::render("psychometrician_cv.rmd",
                   params = list(pdf_mode = FALSE),
                   output_file = "docs/index.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-rmarkdown::render("siraji_cv.rmd",
+rmarkdown::render("psychometrician_cv.rmd",
                   params = list(pdf_mode = TRUE),
                   output_file = tmp_html_cv_loc)
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "cv.pdf")
+                       output = "psychometrician_cv.pdf")
